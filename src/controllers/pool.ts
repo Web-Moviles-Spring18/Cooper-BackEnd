479c2baec496cf2f52404fb5c28ecfb4f0cb3a70
Hermes <hermes.espinola@wizeline.com>
2018-02-18 19:03:06 -0600

    Pool invite links and more properties

--- src/controllers/pool.ts
+++ src/controllers/pool.ts
@@ -1,6 +1,7 @@
-import { default as Pool } from "../models/Pool";
+import { default as Pool, PoolType } from "../models/Pool";
 import { default as User, UserType } from "../models/User";
 import { Request, Response, NextFunction } from "express";
+import { INode } from "neo4js";
 const request = require("express-validator");
 
 /**
@@ -8,8 +9,14 @@ const request = require("express-validator");
  * Create a new pool.
  */
 export let postPool = (req: Request, res: Response, next: NextFunction) => {
-  req.assert("name", "Pool name is not valid").isAlpha();
+  req.assert("name", "Pool name must be a alpha string between 3 and 31 characters").isAlpha().isLength({ min: 3, max: 31 });
   req.assert("private", "Private must be a boolean").isBoolean();
+  req.assert("total", "Total must be a number").isNumeric();
+  req.assert("currency", "Currency must be a length 3 alpha string").isAlpha().isLength({ max: 3, min: 3 });
+  req.assert("paymentMethod", "Payment method must be a alpha string").isAlpha();
+  req.assert("location", "Location must be a LatLnog").optional().isLatLong();
+  req.assert("ends", "Ends must be a date").toDate();
+  req.assert("starts", "Ends must be a date").optional().toDate();
 
   const errors = req.validationErrors();
 
@@ -19,15 +26,33 @@ export let postPool = (req: Request, res: Response, next: NextFunction) => {
 
   const pool = new Pool({
     name: req.body.name,
-    private: req.body.private
+    private: req.body.private,
+    total: req.body.total,
+    currency: req.body.currency,
+    paymentMethod: req.body.paymentMethod,
   });
+
+  if (req.body.location) {
+    pool.location = req.body.location;
+  }
+  if (req.body.starts) {
+    pool.starts = req.body.starts;
+  }
 
   pool.save((err: Error) => {
     if (err) { return next(err); }
     User.findOne({ email: req.user.email }, (err, user: UserType) => {
       if (err) { return next(err); }
       user.owns(pool).then(() => {
-        res.status(200).send("Pool created! kinda");
+        user.participatesIn(pool).then(() => {
+          res.status(200).send({
+            message: "Pool created!",
+            pool
+          });
+        }).catch((err) => {
+          console.error(err);
+          res.status(500).send("Something went wrong.");
+        });
       }).catch((err: Error) => {
         res.status(400).send(err.message);
       });
--- src/models/Pool.ts
+++ src/models/Pool.ts
@@ -2,31 +2,68 @@ import { NextFunction } from "express";
 import { Schema, model } from "../lib/neo4js";
 import { INode } from "neo4js";
 import { UserType } from "./User";
+import * as crypto from "crypto";
 
 export type PoolType = INode & {
   name: string,
   private: boolean,
-  location: string,
-  picture: string
+  invite: String,
+  paymentMethod: "cash" | "credit",
+  currency: "usd" | "mxn",
+  location?: string,
+  starts?: Date,
+  ends: Date,
+  picture?: string
 };
 
 const poolSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    index: true
   },
   private: Boolean,
+  invite: {
+    type: String,
+    required: true,
+    index: true
+  },
   paymentMethod: {
     type: String,
     required: true,
     enum: ["cash", "credit"]
   },
+  currency: {
+    type: String,
+    required: true,
+    enum: ["usd", "mxn"]
+  },
+  total: {
+    type: Number,
+    required: true
+  },
   location: String,
+  starts: Date,
+  ends: {
+    type: Date,
+    required: true
+  },
   picture: String
 });
 
-poolSchema.methods.invite = (user: UserType) => {
+poolSchema.pre("save", function createInvite(next: Function) {
+  const pool: PoolType = this;
+  if (pool._id) { return; } // If the pool already exists don't overwrite the invite link
 
+  // Create a random invite link for this pool
+  crypto.randomBytes(16, (err, buf) => {
+    pool.invite = buf.toString("hex");
+    next();
+  });
+});
+
+poolSchema.methods.invite = (user: UserType) => {
+  // TODO: send email with invite link or a push notification with the invite link?
 };
 
 const Pool = model("Pool", poolSchema);
