7419a70a8f7041600bd17bfcf88507a484cd0a46
Hermes <hermes.espinola@wizeline.com>
2018-03-30 09:30:34 -0700

    Update build script

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -7,6 +7,10 @@ export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 cd $1 && \
 tar zxvf package.tgz -C . && \
 mv dist/package.json . && \
-npm install && \
-cp .env dist && \
-pm2 start dist/server.js -i max — name="cooper-production"
+npm install;
+
+pm2 show cooper-production | grep online;
+if [ $? -eq 0 ] ; then
+  pm2 stop cooper-production
+fi
+pm2 start dist/server.js -i max —-name="cooper-production"
