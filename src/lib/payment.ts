57cb9c0426114b1e99406f5557e25883f2f49648
Hermes <hermes.espinola@wizeline.com>
2018-04-04 01:18:30 -0500

    payment source from req.body

--- src/lib/payment.ts
+++ src/lib/payment.ts
@@ -4,14 +4,14 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 
 type StripeError = Error & { type: string };
 
-export let processPayment = async (user: UserType, charge: { amount: number, currency: "usd" | "mxn", description: string, source: string }): Promise<string> => {
+export let processPayment = async (user: UserType, charge: { amount: number, currency: "usd" | "mxn", description: string, source: string }): Promise<Stripe.charges.ICharge> => {
   if (user.customer && !charge.source) { // User has a card registered.
     const customer = user.customer;
     const source = user.paymentSource;
-    return stripe.charges.create({ ...charge, customer }).then((response: Stripe.charges.ICharge) => response.status);
+    return stripe.charges.create({ ...charge, customer });
   } else if (charge.source) { // Expect to receive a stripe token.
-    stripe.charges.create(charge).then((charge: Stripe.charges.ICharge) => charge.status);
+    return stripe.charges.create(charge);
   } else {
-    throw "No payment method specified";
+    throw new Error("No payment method specified.");
   }
 };
