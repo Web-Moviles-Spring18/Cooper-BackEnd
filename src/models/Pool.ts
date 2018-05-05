13d8bcc00b9658794f54a04d2701bc92a836f688
Hermes <hermes.espinola@wizeline.com>
2018-05-05 00:25:43 -0500

    remove email from invite code

--- src/models/Pool.ts
+++ src/models/Pool.ts
@@ -3,8 +3,6 @@ import { Schema, model } from "../lib/neo4js";
 import { INode } from "neo4js";
 import { UserType } from "./User";
 import * as crypto from "crypto";
-import * as sgMail from "@sendgrid/mail";
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export type PoolType = INode & {
   name: string,
@@ -87,16 +85,6 @@ poolSchema.methods.getTopic = function() {
 poolSchema.methods.inviteUser = function(from: UserType, user: UserType, cb: (err: Error, result: any) => void) {
   const pool: PoolType = this;
   user.invitedTo(pool);
-  const displayName = from.name ? from.name : "Someone";
-  const msg = {
-    to: user.email,
-    subject: `${displayName} invited you to join the ${pool.name} pool!`,
-    from: "service@cooper.mx",
-    text: `Hello,\n\n${displayName} just invited you to join his pool. \n\n` +
-    `If you want to join, please click the following link:\n` +
-    `http://${process.env.HOST_URI}/pool/accept/${pool._id}`
-  };
-  sgMail.send(msg, false, cb);
   cb(undefined, undefined);
 };
 
